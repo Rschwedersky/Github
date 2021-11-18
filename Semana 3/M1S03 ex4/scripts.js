d09163a2fb3fbdb9823736ca0ed8a1ea9090dd55
Rschwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2021-11-18 20:55:16 -0300

    ex4

---
+++ Semana 3/M1S03 ex4/scripts.js
@@ -0,0 +1,11 @@
+var hora = addZero(new Date().getHours());
+var minuto = addZero(new Date().getMinutes());
+
+function addZero(i) {
+    if (i < 10) {i = "0" + i}
+    return i;
+  }
+console.log(hora);
+console.log(minuto);
+document.getElementById("relogio").innerHTML = `São ${hora}:${minuto}`
+
