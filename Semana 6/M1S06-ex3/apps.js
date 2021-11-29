6dc4d3b5422280bc82eddfeaad1687516db21b6d
Rschwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2021-11-29 18:15:47 -0300

    ex3

---
+++ Semana 6/M1S06-ex3/apps.js
@@ -0,0 +1,7 @@
+let arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arrayInvertido = [];
+
+arrayNumeros.forEach((item) => arrayInvertido.unshift(item));
+
+console.log(arrayNumeros);
+console.log(arrayInvertido);
