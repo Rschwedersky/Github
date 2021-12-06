03c1e9e81bac75f6493395e749e2cf238fda8d0a
Rschwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2021-12-06 18:56:16 -0300

    ex6

--- Semana 7/M1S07-ex6/app.js
+++ Semana 7/M1S07-ex6/app.js
@@ -1,9 +1,21 @@
-async function euNaoEspero(){
-        await aguarda5Segundos();
-        console.log('Eu não espero')
+const botao = document.getElementById('add');
+const input = document.getElementById('campoCep');;
+const resultado = document.getElementById('resultado');
+botao.addEventListener('click', consultaCep)
+let cep = '01001000';
+async function consultaCep(){
+       try {  const cep = input.value
+              const url = `https://viacep.com.br/ws/${cep}/json`
+              const response = await fetch(url)
+              const json = await response.json();
+              console.log(json);
+              resultado.innerText = `Seu endereço é: ${json.logradouro} no Bairro ${json.bairro}`;
+              
+       return json;
 }
-async function aguarda5Segundos() {
-        await new Promise(resolve => setTimeout(resolve, 3000)); // 3 sec
-        console.log("Função diz: Terminei!")
-    }
-    euNaoEspero();
+catch(error){console.log(error)}
+}
+
+
+
+
