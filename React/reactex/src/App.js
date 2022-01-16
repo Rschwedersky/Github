4b0ede830962e7f4457b22e3527073d4f55a3974
Rschwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-01-16 20:36:55 -0300

    ex4

--- React/reactex/src/App.js
+++ React/reactex/src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { useReducer, useState } from 'react';
 
-const initialState = {result:0};
 
-
+const initialState = { result:0};
 const reducer = (state, { type, payload }) => {
+  console.log('reducer',type);
   switch (type) {
     case 'add':
       return { result: state.result + payload.valor };
+    case 'sub':
+      return { result: state.result - payload.valor };
+    case 'mult':
+      return { result: state.result * payload.valor };
+    case 'div':
+      return { result: state.result / payload.valor };
     default:
       return state;
   }
@@ -17,6 +23,8 @@ const reducer = (state, { type, payload }) => {
 
 function Counter() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [inputNumero, setInputNumero] = useState();
+  const [inputOperacao, setInputOperacao] = useState('add');
 
   
   return (
@@ -24,18 +32,18 @@ function Counter() {
       
       <label style={{ paddingBottom: '10px' }}>
           Numero:
-          <input type="number" value={state?.valor} onChange={(e) => dispatch({ payload:{ valor: parseInt(e.target.value) }})} />
+          <input type="number" value={state?.valor} onChange={(e) => setInputNumero(parseInt(e.target.value))} />
         </label>
         <label style={{ paddingBottom: '10px' }}>
           Operação:
-          <select value={state?.type} onChange={(e) => dispatch({ type: (e.target.value)})} >
+          <select value={state?.type} onChange={(e) => {setInputOperacao(e.target.value);console.log('inputV',typeof(inputOperacao))}} >
             <option value={"add"}>add</option>
             <option value={"sub"}>sub</option>
             <option value={"mult"}>mult</option>
             <option value={"div"}>div</option>
           </select>
         </label>
-
+        <button onClick={() => dispatch({type: inputOperacao, payload:{ valor:inputNumero} })}>Resultado</button>
         <p>{`Resultado: ${state?.result}`}</p>
     </>
   );
