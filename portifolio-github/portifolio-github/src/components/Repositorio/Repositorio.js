c285111b823a8e49ea290e0ae6f32ced3ccebaa0
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-01-20 11:19:42 -0300

    ex10

--- portifolio-github/portifolio-github/src/components/Repositorio/Repositorio.js
+++ portifolio-github/portifolio-github/src/components/Repositorio/Repositorio.js
@@ -2,11 +2,11 @@ import PropTypes from "prop-types";
 import styles from "./Repositorio.module.css"
 
 
-export const Repositorio = ({dados, destacar}) => {
+export const Repositorio = ({dados, destacar, func}) => {
   return (
     <>
     {dados.map((item) => (
-    <article className={styles.card} style={destacar===item.id?{backgroundColor: "lightblue"  }:{ }} key={item.id}>
+    <article onClick={() => func(item.id)} className={styles.card} style={destacar===item.id?{backgroundColor: "lightblue"  }:{ }} key={item.id}>
       <h3>{item.name}</h3>
       <p>{item.description}</p>
       </article> ))}
