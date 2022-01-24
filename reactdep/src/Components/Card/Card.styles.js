5ae0f808ad335d5d70269e01f9288b88325dbfdd
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-01-24 16:53:14 -0300

    last

--- reactdep/src/Components/Card/Card.styles.js
+++ reactdep/src/Components/Card/Card.styles.js
@@ -4,7 +4,7 @@ export const Botao = styled.button`
   padding: 5px;
   background-color: ${({ theme, dark }) => dark?theme.colors.secondary.dark:theme.colors.secondary.light};
   cursor: pointer;
-  color: ${({ theme, seguindo }) => seguindo?theme.colors.primary.dark:theme.colors.primary.light};
+  color: ${({ theme, seguindo }) => seguindo?'#7f0000':'blue'};
   border: 2px solid #37474f;
   border-radius: 3px;
   &:hover {
